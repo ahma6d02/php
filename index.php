4ece29bfd72ad53329f91859d9ea800a00743a9f
ahma6d02 <95081829+ahma6d02@users.noreply.github.com>
2023-02-07 12:46:54 +0200

    AAAA

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+//$block_ips = array("xxx.xxx.xxx.xxx", "yyy.yyy.yyy.yyy"); // Replace with the IP addresses you want to block
+$client_ip = 99999999;
+
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>php</title>
+</head>
+<body>
+<h1>php</h1> 
+<h1><?php echo $client_ip;?></h1>
+
+
+</body>
+</html>
